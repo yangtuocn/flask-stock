980d0194a79be0273b4623f951bd79975fc37232
yangtuocn <yangtuocn@gmail.com>
2017-03-19 16:07:53 -0700

    third commit

--- app.py
+++ app.py
@@ -27,5 +27,5 @@ def add_query():
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    port = int(os.enviorn.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host = '0.0.0.0', port = port)
