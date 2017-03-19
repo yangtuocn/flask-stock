75835f36a41701f90e6b28e9e858a8ac7b1e53e2
yangtuocn <yangtuocn@gmail.com>
2017-03-19 16:51:32 -0700

    4

--- app.py
+++ app.py
@@ -20,12 +20,14 @@ def index():
 def add_query():
     quandl.ApiConfig.api_key = 'oqWh8vNGwgZkRk4PvsyQ'
     data = quandl.get_table('WIKI/PRICES', ticker = request.form['stock'])
-    output_file('plot.html')
+    output_file('templates\plot.html')
     p = figure()
     p.line(range(len(data)), list(data['close']))
-    show(p)
-    return redirect(url_for('index'))
+    # show(p)
+    # return redirect(url_for('plot'))
+    return render_template('plot.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host = '0.0.0.0', port = port)
+    # app.run()
