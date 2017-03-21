b9c038f8d8c15008a35ccf35254eafa764bb150e
yangtuocn <yangtuocn@gmail.com>
2017-03-20 21:36:40 -0700

    6

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from flask import Flask, request, session, g, redirect, url_for, \
-                  abort, render_template, flash, jsonify
+                  abort, render_template, flash, jsonify, render_template_string
 import os
 import quandl
 import requests
 import simplejson as json
 import pandas as pd
 from bokeh.plotting import figure, output_file, show, save
+from bokeh.embed import components
+from jinja2 import Markup
 
 DEBUG = True
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-output_file('templates\plot.html')
-p = figure(x_axis_type = "datetime")
 
 @app.route('/')
 def index():
@@ -23,11 +22,10 @@ def index():
 def add_query():
     quandl.ApiConfig.api_key = 'oqWh8vNGwgZkRk4PvsyQ'
     data = quandl.get_table('WIKI/PRICES', ticker = request.form['stock'])
+    p = figure(x_axis_type = "datetime")
     p.line(list(data['date']), list(data['close']))
-    save(p)
-    # show(p)
-    # return redirect(url_for('plot'))
-    return render_template('plot.html')
+    scr, di = components(p)
+    return render_template('index.html', plot_script=Markup(scr), plot_div=Markup(di))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
